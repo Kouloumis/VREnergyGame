cb0f6cfa4c7f0c7706b6106c55b4c3442c1553e7
Kouloumis <antkoul71@gmail.com>
2018-11-21 16:34:35 +0100

    Third commit

---
+++ Assets/Scripts/NavigateToScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigateToScene : MonoBehaviour {
+
+	[SerializeField]
+	string SceneToNavigateTo;
+
+	public void Navigate()
+	{
+		SceneManager.LoadScene(SceneToNavigateTo);
+	}
+
+}
