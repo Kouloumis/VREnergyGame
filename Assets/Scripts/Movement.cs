cfdad7b53d1575f97f37918c110283c1825168ee
Kouloumis <antkoul71@gmail.com>
2018-11-28 15:52:47 +0100

    CubeTest

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+
+	}
+
+	void FixedUpdate()
+	{
+
+	// MOVEMENT ON THE X AXIS
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidBody.AddForce (movement * speed);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
